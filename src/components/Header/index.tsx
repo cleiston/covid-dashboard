7c1c8edfb2ed044fcd2f145f38c3784e00e2f4f7
Cleiston <cleistonrs@gmail.com>
2020-07-11 10:25:41 -0300

    small fix in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,16 +8,20 @@ const Header: React.FC = () => {
     // this state will be used to toggle the navigation bar (for mobile devices, below 600px width)
     const [displayMenu, setDisplayMenu] = useState<boolean>(false);
 
+    function handleToggle(){
+        setDisplayMenu(prevState => !prevState);
+    }
+
     return (
-        <Container mobileMenu={displayMenu}>
+        <Container mobileMenu={displayMenu} onBlur={handleToggle}>
             <Title><Up /> covid dashboard</Title>
             <Menu>
                 <MenuItem href="/"><Home /> Home </MenuItem>
                 <MenuItem href="#about"><About /> About</MenuItem>                
             </Menu>
             { displayMenu ? 
-                <XIcon onClick={() => setDisplayMenu(!displayMenu)} /> :
-                <MenuIcon onClick={() => setDisplayMenu(!displayMenu)} />
+                <XIcon onClick={handleToggle} /> :
+                <MenuIcon onClick={handleToggle} />
             }
             
         </Container>
